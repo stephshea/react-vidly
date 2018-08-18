cd3765997dde94f50f3c867f355e39293c089984
Stephanie <stephcshea@gmail.com>
2018-08-18 07:30:38 -0400

    created movies component

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <main role="main" class="container">
+      <main className="main" class="container">
       <h1>I am Abby</h1>
       </main>
     );
